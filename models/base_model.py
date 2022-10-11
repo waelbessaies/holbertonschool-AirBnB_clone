3beea78fcff40eb7fe31c4e3efc01697d1c21c2e
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-11 14:54:34 +0200

    test_base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 from datetime import datetime
 from uuid import uuid4
 
+
 class Basemodel:
     """a class"""
 
@@ -19,7 +20,16 @@ def __str__(self):
     return "[{}] ({}) {}".format(
         self.__class__.__name__, self.id, self.__dict__)
 
-def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
-        self.updated_at = datetime.now()
 
+def save(self):
+    """updates the public instance attribute updated_at with the current datetime"""
+    self.updated_at = datetime.now()
+
+
+def to_dict(self):
+    """returns a dictionary containing all keys/values of __dict__ of the instance"""
+    my_dict = self.__dict__.copy()
+    my_dict['__class__'] = self.__class__.__name__
+    my_dict['created_at'] = self.created_at.isoformat()
+    my_dict['updated_at'] = self.updated_at.isoformat()
+    return my_dict
