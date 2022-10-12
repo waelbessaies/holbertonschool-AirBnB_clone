94971d5b0943ab72cddc75839cf9541d59250f08
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-13 01:00:28 +0200

    console.py

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" this Module contains the entry point of the command interpreter """
+
+import cmd
+import sys
+
+
+
+class HBNBCommand(cmd.Cmd):
+    """ the class HBNBCommand """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, args):
+        """ to exit the comand line"""
+        return True
+
+    def do_quit(self, args):
+        """ to quit the promt"""
+        return True
+
+    def emptyline(self):
+        """ if no argument given """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
