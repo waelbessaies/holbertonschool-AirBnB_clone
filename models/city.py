d286637ae308340aab878da057cba32ab7baed98
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-13 12:40:58 +0200

    city.py

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this is a comment """
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ this is the class City """
+
+    state_id = ""
+    name = ""
