0122c5425b90baafb5640093a54af3c367e0c9de
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-16 20:00:54 +0200

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 class FileStorage:
     """Filestorage class"""
     __file_path = "file.json"
@@ -18,7 +19,20 @@ class FileStorage:
     def all(self):
         """Returns objects of the dictionary"""
         return self.__objects
-    
+
     def new(self, obj):
         """Adds a new object in objects"""
         self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """"serializes __objects to the JSON file"""
+        dict = {}
+        for key, value in self.__objects.items():
+            dict[key] = value.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(dict, f)
+
+    def reload(self):
+        """deserializes the JSON file"""
+        with open(self.__file_path, 'r') as f:
+            self__objects = json.load(f)
